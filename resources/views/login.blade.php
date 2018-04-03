4b9845feff5e06d9f3cb68c0b62f86c6bd6e19b6
570504071 <shenjianlin@datartisan.com>
2018-04-03 18:14:30 +0800

    添加登陆相关视图

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Welcome to Login</title>
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+    <section class="container">
+        <div class="login">
+            <h1>Please Login</h1>
+            <form name="login" method="POST" action="{{ route('postlogin') }}">
+                {{ csrf_field() }}
+                <p><input type="text" name="email" value="" placeholder="Email"></p>
+                <p><input type="password" name="password" value="" placeholder="Password"></p>
+                <p class="submit"><input type="submit" name="commit" value="Login"></p>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
