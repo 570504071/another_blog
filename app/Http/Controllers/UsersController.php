ed0f0ad64b2af367a63b561e458af9f7385a1859
e <570504071@qq.com>
2018-04-03 21:33:43 +0800

    添加注册功能并修改路由

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,21 +7,56 @@ use Auth;
 
 class UsersController extends Controller
 {
-    //
-    public function postLogin(Request $request)
+    public function showSignupForm(Request $request)
     {
-        Auth::attempt([
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ]);
-
-        return redirect()->to('add_new_post');
+        return view('signup');
     }
 
-    public function getLogout(Request $request)
+    public function signup(Request $request)
+    {
+        // dd($request->all());
+        $messages = [
+            'password.required' => '请填写您的密码',
+            'password.confirmed' => '密码不一致',
+            'password.min' => '密码最短为4位',
+        ];
+
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required|min:4|confirmed',
+        ], $messages);
+
+        User::create([
+            'email' => $input['email'],
+            'password' => $input['password'],
+        ]);
+
+        return redirect()->to('get_login');
+    }
+
+    public function showLoginForm(Request $request)
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt([
+                'name' => $request::input('name');
+                'email' => $request::input('email');
+            ])
+        {
+            return redirect()->to('get_add');
+        }
+
+        return redirect()->to('get_login');
+    }
+
+    public function logout()
     {
         Auth::logout();
-
-        return redirect()->to('index');
+        return redirect('/');
     }
 }
