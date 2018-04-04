840c56d5b7d8e8b895222f69c1ad8a868cc4e2a9
570504071 <shenjianlin@datartisan.com>
2018-04-04 15:05:44 +0800

    更改排版和添加退出按钮

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,42 @@
 <!doctype html>
 <html lang="en">
 <head>
-<meta charset="utf-8" />
-<title>My Awesome Blog</title>
-<link rel="stylesheet" href="/assets/blog/css/styles.css" type="text/css" media="screen" />
-<link rel="stylesheet" type="text/css" href="/assets/blog/css/print.css" media="print" />
-<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <meta charset="utf-8" />
+    <title>My Awesome Blog</title>
+    <link rel="stylesheet" href="/assets/blog/css/styles.css" type="text/css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/assets/blog/css/print.css" media="print" />
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
+
 <body>
 <div id="wrapper">
-<header>
-<h1><a href="/">My Awesome Blog</a></h1>
-<p>Welcome to my awesome blog</p>
-</header>
-<section id="main">
-<section id="content">
-@foreach($posts as $post)
-    <article>
-        <h2>{{$post->title}}</h2>
-        <p>{{$post->content}}</p>
-        <p><small>Posted by <b>{{$post->Author->name}}</b> at <b>{{$post->created_at}}</b></small></p>
-    </article>
-@endforeach
-</section>
-</section>
-<footer>
+    <header>
+        <h1><a href="{{ route('user_profile') }}">My Awesome Blog</a></h1>
+        <p>Welcome to my awesome blog</p>
+    </header>
+
+    <form action="{{ route('logout') }}" method="post">
+        {{ csrf_field() }}
+        这里是<button type="submit">退出</button>按钮
+    </form>
+
+    <section id="main">
+        <section id="content">
+            已发布的文章如下
+            @foreach($posts as $post)
+                <article>
+                    <ul>
+                        <h2>标题：{{$post->title}}</h2>
+                        <p>内容：{{$post->content}}</p>
+                        <p><small>Posted by <b>{{$post->Author->name}}</b> at <b>{{$post->created_at}}</b></small></p>
+                    </ul>
+                    <hr>
+                </article>
+            @endforeach
+        </section>
+    </section>
+
+    <footer>
         <section id="footer-area">
             <section id="footer-outer-block">
                 <aside class="footer-segment">
@@ -33,6 +45,6 @@
             </section>
         </section>
     </footer>
-    </div>
-    </body>
-    </html>
+</div>
+</body>
+</html>
