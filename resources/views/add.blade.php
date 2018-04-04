f0b3414ad5866773ec6183e10d69122da32a0e0a
570504071 <shenjianlin@datartisan.com>
2018-04-04 13:40:48 +0800

    添加退出登陆的按钮

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -8,7 +8,10 @@
 <section class="container">
     @if(Auth::check())
     <div class="content">
-        <h1>这里是添加文章页面，欢迎您的投稿， {{Auth::user()->name}} ! - <b>这里是退出按钮</b></h1>
+        <h1>这里是添加文章页面，欢迎您的投稿， {{Auth::user()->name}} ! -
+            <form action="{{ route('logout') }}" method="post">
+                {{ csrf_field() }}
+                这里是<button type="submit">退出</button>按钮</form></h1>
         <form name="add_post" method="POST" action="{{ route('post_add') }}">
             {{ csrf_field() }}
             <p><input type="text" name="title" placeholder="Post Title" value=""/></p>
